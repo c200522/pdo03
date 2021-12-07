a630a2395a735db4f4050283e0a4cd493ec4cc9f
c200522 <84229952+c200522@users.noreply.github.com>
2021-12-07 15:17:46 +0900

    Update complete_u.php

--- complete_u.php
+++ complete_u.php
@@ -3,7 +3,13 @@
   require_once("functions.php");
 
   /* (1)ここにセッション変数の受け渡し処理を追加して下さい  */
+$edit = $_SESSION['edit']; 
 
+$name = $_SESSION['name']; 
+
+$email = $_SESSION['email']; 
+
+$gender = $_SESSION['gender']; 
 
 
 
@@ -11,6 +17,7 @@
 $dbh = db_conn();      // データベース接続
 try{
     /* (2)ここにSQL文＋プレースホルダー　の文字列を準備する処理を追加して下さい  */
+	$sql = "UPDATE user SET email = :email, name = :name, gender = :gender, updatedate = NOW() WHERE id = :id";
 
     $stmt = $dbh->prepare($sql);                           //クエリの実行準備
     $stmt->bindValue(':email', $email, PDO::PARAM_STR);    //バインド:プレースホルダ―の値を埋める
